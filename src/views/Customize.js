35cb05a827189cbe91cb6e89b4a54a3e8f1b4466
Kerns Phoegon (PC-Laptop) <k.phoegon@gmail.com>
2019-06-17 14:13:12 -0600

    Basic Selection Setup

--- src/views/Customize.js
+++ src/views/Customize.js
@@ -22,7 +22,35 @@ export default class Customize extends Component {
                     <div id="Customimg" style={{ float: "right", height: "300px", width: "60%", backgroundColor: "aqua" }}>
                 det det det
                 </div>
-                    <div style={{ float: "left", height: "300px", width: "40%", backgroundColor: "purple" }}>
+                    <div style={{ float: "left", height: "300px", width: "40%", backgroundColor: "purple", textAlign: "left" }}>
+                        <p className="selword">Size:</p>
+                        <select className="choosers">
+                            <option value="10inch">10" Inches</option>
+                            <option value="12inch">12" Inches</option>
+                            <option value="14inch">14" Inches</option>
+                            <option value="16inch">16" Inches</option>
+                        </select>
+                        <p className="selword">Sause:</p>
+                        <select className="choosers">
+                            <option value="regularsa">Regular</option>
+                            <option value="lightreg">Light</option>
+                            <option value="nonesa">No Sause</option>
+                            <option value="specwhite">White Cream</option>
+                        </select> <br />
+                            <p className="selword">Cut:</p>
+                        <select className="choosers">
+                            <option value="regularcu">Normal 8-Slices</option>
+                            <option value="half">Four Corners 4-Slices</option>
+                            <option value="hex">Wagon Wheel 6-Slices</option>
+                            <option value="nonecu">No Cut</option>
+                        </select> <br />
+                                <p className="selword">Cheese:</p>
+                        <select className="choosers">
+                            <option value="regularch">Regular</option>
+                            <option value="extra">Extra</option>
+                            <option value="light">Light</option>
+                            <option value="nonech">None</option>
+                        </select>
                     </div>
                 aTHeEkraldferwaer dfwe
                    <br />wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer aTHeEkraldferwaer dfwe wrwe fd werwedfer q wwerwer qer 
