8ca6afa327177d5335cabbf22c0bac388da58192
Kerns Phoegon (PC-Laptop) <k.phoegon@gmail.com>
2019-06-17 10:48:43 -0600

    Deeper Deerp Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Load in other views here
+import Customize from './views/Customize';
+
+
+// Very simple layout for now; Eventually need to set up navigation
+function App() {
+  return (
+    <Customize />
+
+  );
+}
+
+export default App;
+//test
